44e913977c0e20d8520732bb7e0d5d02f03fa42d
Matheus Motta <66272916+Mmotta18@users.noreply.github.com>
2020-09-02 16:33:20 -0300

    Updates

---
+++ precache-manifest.e46154840fd1a77b91a0a176ced92dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380c3545561397457b310909530feb85",
+    "url": "/TesteReact/index.html"
+  },
+  {
+    "revision": "fa5a1e22befef4b27baa",
+    "url": "/TesteReact/static/css/main.f98756ae.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/TesteReact/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "fa5a1e22befef4b27baa",
+    "url": "/TesteReact/static/js/main.94f4c412.chunk.js"
+  },
+  {
+    "revision": "4b922eedbe729a81bc42",
+    "url": "/TesteReact/static/js/runtime~main.1e8c17df.js"
+  }
+]);
